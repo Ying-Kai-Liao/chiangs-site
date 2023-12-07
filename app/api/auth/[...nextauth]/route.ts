6965cb122163dda290b5da7bd7b09321ecfee1ca
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-12-07 16:54:04 +0800

    auth update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,4 @@
-import { handlers } from "@/actions/auth";
+import NextAuth from "next-auth"
+import { authOptions } from "@/actions/auth"
+const { handlers } = NextAuth(authOptions)
 export const { GET, POST } = handlers
