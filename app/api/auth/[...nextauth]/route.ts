c0040a33f789a89a61321374525b1017105795b8
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-12-07 17:25:29 +0800

    important update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
 import NextAuth from "next-auth"
 import { authOptions } from "@/actions/auth"
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions) // this is so fucking important
 export const { GET, POST } = handler
