de09f1d02aa379f44f1e3b12674ca8ac95c06f43
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-12-07 17:04:18 +0800

    bug fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
 import NextAuth from "next-auth"
 import { authOptions } from "@/actions/auth"
 const { handlers } = NextAuth(authOptions)
-export const { GET="get bad", POST } = handlers
+export const { GET, POST } = handlers || {}
